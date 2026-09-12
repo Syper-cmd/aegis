15cfb304490bf1491188efe206b24fa32162fd32
Александр Пеливан <infosajta83@gmail.com>
2026-09-12 10:03:10 +0300

    Add WritePauload method

--- internal/pe/injector.go
+++ internal/pe/injector.go
@@ -3,6 +3,7 @@ package pe
 import (
 	"debug/pe"
 	"fmt"
+	"os"
 )
 
 type Injector struct {
@@ -12,8 +13,8 @@ type Injector struct {
 
 type SectionData struct {
 	Name        string // Ім'я секції
-	PhysSize    uint32 // Фізичний розмір данних
-	VirtualSize uint32 // "Віртуальний" розмір данних
+	PhysSize    uint32 // Фізичний розмір даних
+	VirtualSize uint32 // "Віртуальний" розмір даних
 	Offset      uint32 // Зсув для пошуку секції
 	CaveOffset  uint32 // Зсув каверни
 	CaveSize    int    // Розмір каверни
@@ -22,7 +23,7 @@ type SectionData struct {
 func New(filePath string) (*Injector, error) {
 	peFile, err := pe.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("Виникла помилка відкриття PE файлу: %v", err)
+		return nil, fmt.Errorf("виникла помилка відкриття PE файлу: %w", err)
 	}
 
 	return &Injector{
@@ -31,42 +32,22 @@ func New(filePath string) (*Injector, error) {
 	}, nil
 }
 
-func (inj Injector) Close() error {
+// Всі методи робимо з ресивером (inj *Injector)
+func (inj *Injector) Close() error {
 	if inj.peFile != nil {
-		return inj.peFile.Close()
+		err := inj.peFile.Close()
+		inj.peFile = nil
+		return err
 	}
-
-	return fmt.Errorf("Помилка! PE файл не був відкритий")
-}
-
-func (inj Injector) GetLargestCavern() (*SectionData, error) {
-	sections, err := inj.GetSectionDebugInfo()
-
-	if err != nil {
-		return nil, fmt.Errorf("Виникла помилка отримання секцій: %v", err)
-	}
-
-	largestSection := sections[0]
-
-	for _, secData := range sections[1:] {
-		if secData.CaveSize > largestSection.CaveSize {
-			largestSection = secData
-		}
-	}
-
-	return &largestSection, nil
-}
-
-func (inj Injector) WritePayload() error {
-
 	return nil
 }
 
-func (inj Injector) GetSectionDebugInfo() ([]SectionData, error) {
+func (inj *Injector) GetSectionDebugInfo() ([]SectionData, error) {
 	if inj.peFile == nil {
-		return nil, fmt.Errorf("Помилка! PE файл не було відкрито.")
+		return nil, fmt.Errorf("помилка! PE файл не було відкрито")
 	}
-	results := []SectionData{}
+
+	var results []SectionData
 
 	for _, sec := range inj.peFile.Sections {
 		cavernSize := -1
@@ -86,4 +67,61 @@ func (inj Injector) GetSectionDebugInfo() ([]SectionData, error) {
 	}
 
 	return results, nil
+}
+
+func (inj *Injector) GetLargestCavern() (*SectionData, error) {
+	sections, err := inj.GetSectionDebugInfo()
+	if err != nil {
+		return nil, fmt.Errorf("виникла помилка отримання секцій: %w", err)
+	}
+
+	if len(sections) == 0 {
+		return nil, fmt.Errorf("у PE файлі відсутні секції")
+	}
+
+	var largestSection *SectionData
+
+	for i := range sections {
+		// Шукаємо тільки дійсні каверни
+		if sections[i].CaveSize > 0 {
+			if largestSection == nil || sections[i].CaveSize > largestSection.CaveSize {
+				largestSection = &sections[i]
+			}
+		}
+	}
+
+	if largestSection == nil {
+		return nil, fmt.Errorf("підходящих каверн (Slack Space) не знайдено")
+	}
+
+	return largestSection, nil
+}
+
+func (inj *Injector) WritePayload(caveOffset uint32, payload []byte) error {
+	if inj.peFile != nil {
+		_ = inj.peFile.Close()
+		inj.peFile = nil
+	}
+
+	file, err := os.OpenFile(inj.filePath, os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("помилка відкриття файлу на запис: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Seek(int64(caveOffset), 0)
+	if err != nil {
+		return fmt.Errorf("помилка позиціонування Seek: %w", err)
+	}
+
+	written, err := file.Write(payload)
+	if err != nil {
+		return fmt.Errorf("помилка запису даних: %w", err)
+	}
+
+	if written != len(payload) {
+		return fmt.Errorf("записано частково: %d з %d байт", written, len(payload))
+	}
+
+	return file.Sync()
 }
